45a8192fdd1ff899a2ab7a0717cc0035ae8b4834
Bancie <144613141+Bancie@users.noreply.github.com>
2025-10-03 16:50:14 +0700

    react

---
+++ expense-manager-rollup/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HelloWorld from './component/HelloWorld';
+
+ReactDOM.render(
+   <React.StrictMode>
+      <HelloWorld />
+   </React.StrictMode>,
+   document.getElementById('root')
+);
--- react/src/Mess.tsx
+++
@@ -1,6 +0,0 @@
-function test () {
-    const name = 'Bang';
-    return <h1> {name} Nguyen</h1>;
-}
-
-export default test;
